7d1f7d62c45a28a9bad32cbf8a87c95eafe966e8
seryafarma <enhope.estheim@gmail.com>
2023-02-27 22:32:53 +0100

    Array instead of hardcoded copy paste.

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -9,55 +9,54 @@ const humElement1 = document.getElementById("hum1");
 const tempElement2 = document.getElementById("temp2");
 const humElement2 = document.getElementById("hum2");
 
+const dbPathTemp = ["device_1/temperature", "device_2/temperature"]
+const dbPathHum = ["device_1/humidity", "device_2/humidity"]
+
+var mappings =
+  [
+    {
+      "element": document.getElementById("temp1"),
+      "references": firebase.database().ref().child(dbPathTemp[0])
+    },
+    {
+      "element": document.getElementById("temp2"),
+      "references": firebase.database().ref().child(dbPathTemp[1])
+    },
+    {
+      "element": document.getElementById("hum1"),
+      "references": firebase.database().ref().child(dbPathHum[0])
+    },
+    {
+      "element": document.getElementById("hum2"),
+      "references": firebase.database().ref().child(dbPathHum[1])
+    }
+  ]
+
 // MANAGE LOGIN/LOGOUT UI
 const setupUI = (user) => {
   if (user) {
     //toggle UI elements
     loginElement.style.display = 'none';
     contentElement.style.display = 'block';
-    authBarElement.style.display ='block';
-    userDetailsElement.style.display ='block';
+    authBarElement.style.display = 'block';
+    userDetailsElement.style.display = 'block';
     userDetailsElement.innerHTML = user.email;
 
     // get user UID to get data from database
     var uid = user.uid;
     console.log(uid);
 
-    // Database paths (with user UID)
-    var dbPathTemp1 = 'device_1/temperature';
-    var dbPathHum1 = 'device_1/humidity';
-    var dbPathTemp2 = 'device_2/temperature';
-    var dbPathHum2 = 'device_2/humidity';
-
-    // Database references
-    var dbRefTemp1 = firebase.database().ref().child(dbPathTemp1);
-    var dbRefHum1 = firebase.database().ref().child(dbPathHum1);
-    var dbRefTemp2 = firebase.database().ref().child(dbPathTemp2);
-    var dbRefHum2 = firebase.database().ref().child(dbPathHum2);
-
-    // Update page with new readings
-    dbRefTemp1.on('value', snap => {
-      tempElement1.innerText = snap.val();
-    });
-
-    dbRefHum1.on('value', snap => {
-      humElement1.innerText = snap.val();
-    });
-
-    dbRefTemp2.on('value', snap => {
-      tempElement2.innerText = snap.val();
-    });
-
-    dbRefHum2.on('value', snap => {
-      humElement2.innerText = snap.val();
-    });
-
-  // if user is logged out
-  } else{
+    for (const each of mappings) {
+      each.references.on('value', snap => {
+        each.element.innerText = snap.val();
+      });
+    }
+    // if user is logged out
+  } else {
     // toggle UI elements
     loginElement.style.display = 'block';
-    authBarElement.style.display ='none';
-    userDetailsElement.style.display ='none';
+    authBarElement.style.display = 'none';
+    userDetailsElement.style.display = 'none';
     contentElement.style.display = 'none';
   }
 }
